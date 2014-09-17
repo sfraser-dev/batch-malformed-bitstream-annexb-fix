cff4fe14aa2dea84bd662c563f48e1d2eb6e4090
stewart fraser <toepoke@hotmail.com>
2014-09-17 16:06:58 +0100

    Adding folder structure to better store video files.

--- workFolder/HarkandMalformedBitstreamAutoRename.bat
+++ workFolder/HarkandMalformedBitstreamAutoRename.bat
@@ -8,9 +8,10 @@ goto:eof
 :: Get filename from 1st argument
 SET ORIG_NAME="%~f1"
 :: New name via expansion of drive, path, name and extension
-SET NEW_NAME="%~dpn1_new%~x1"
+:: SET NEW_NAME="%~dpn1_new%~x1"
+SET TS_NAME="%~dpn1_new%.ts"
 echo %ORIG_NAME%
-echo %NEW_NAME%
+echo %TS_NAME%
 :: Execute the FFMPEG command to convert byte-stream to Annex B format
-ffmpeg -i %ORIG_NAME% -c:a copy -c:v copy -bsf h264_mp4toannexb -f mpegts %NEW_NAME%
+::ffmpeg -y -i %ORIG_NAME% -c:a copy -c:v copy -bsf h264_mp4toannexb -f mpegts %TS_NAME%
 goto:eof
