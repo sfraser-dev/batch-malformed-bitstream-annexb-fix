5a32a587a6606459db899939ab26958e7ec49f46
stewart fraser <toepoke@hotmail.com>
2014-09-25 12:31:08 +0100

    Minor.

--- workFolder/HarkandMalformedBitstreamAutoRename.bat
+++ workFolder/HarkandMalformedBitstreamAutoRename.bat
@@ -1,7 +1,6 @@
 @echo off
-rem FOR /r %%i in (*.mp4) DO ffmpeg -i "%%~fi"  -c:a copy -c:v copy -bsf h264_mp4toannexb -f mpegts -c:a copy -c:v copy "%%~dpni(2)%%~xi"
 
-FOR /f "delims=" %%f in ('dir "*.mp4" /s/b') do @call:runItFun "%%f"
+for /f "delims=" %%f in ('dir "*.mp4" /s/b') do @call:runItFun "%%f"
 goto:eof
 
 :runItFun
@@ -14,4 +13,5 @@ echo %ORIG_NAME%
 echo %TS_NAME%
 :: Execute the FFMPEG command to convert byte-stream to Annex B format
 ::ffmpeg -y -i %ORIG_NAME% -c:a copy -c:v copy -bsf h264_mp4toannexb -f mpegts %TS_NAME%
+ffmpeg -y -i %ORIG_NAME% -c:a copy -c:v copy -f mpegts %TS_NAME%
 goto:eof
